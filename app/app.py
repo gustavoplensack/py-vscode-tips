4426d13bd53f6244f5b0787a4b65acb1df860f91
gustavo plensack <gustavoplensack@gmail.com>
2022-12-06 08:00:40 -0300

    fix: use portuguese for route name

--- app/app.py
+++ app/app.py
@@ -16,7 +16,7 @@ SUM_SERVICE = SumService()
 def health_check():
     return {"message":"hello world!"}
 
-@app.post("/sum")
+@app.post("/soma")
 def sum_route():
     
     sum_request = SumRequest(**request.get_json())
